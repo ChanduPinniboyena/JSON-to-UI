2be2d642854ec2612047b9bbf281d342a1509d32
ChanduPinniboyena <pinniboyenachandu2018@gmail.com>
2025-12-18 13:34:30 +0530

    Cleared Database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -9,8 +8,14 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
 });
 
-// Test connection
+// pool.connect():
+// We do NOT use this here. That would be like manually opening the door.
+// Instead, we let the pool automatically open connections when needed (Lazy Loading).
+
+// This is like installing a doorbell. It just waits and listens.
+// It fires ONLY when the pool successfully creates a new connection for a query.
 pool.on('connect', () => {
+  console.log("Get req from api")
   console.log('✓ Connected to PostgreSQL database');
 });
 
